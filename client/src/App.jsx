b2305b686e751ebf9e3f62e0d3ef2c90743849c9
fmfahath <fmfahath.dev@gmail.com>
2025-07-12 11:52:11 +0530

    client --  google font added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,8 @@ function App() {
 
   return (
     <>
-      <h1 className="text-blue-500 text-2xl">Hello TechCodz!</h1>
+      <h1 className="text-blue-500 text-2xl ">Hello TechCodz!</h1>
+      <p>hello fahath</p>
     </>
   )
 }
