332478e88a378a48575980b94ab4788738329392
fmfahath <fmfahath.dev@gmail.com>
2025-07-22 12:43:51 +0530

    client -- searcBar component added in Home.js, mobile responsive added, useNavigate route defined

--- client/src/components/student/SearchBar.jsx
+++ client/src/components/student/SearchBar.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Search } from 'lucide-react'
+import { useNavigate } from 'react-router-dom'
 
 const SearchBar = () => {
+
+    const navigate = useNavigate()
+    const [searchInput, setSearchInput] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        navigate('/course-list/' + searchInput)
+    }
+
     return (
-        <div>SearchBar</div>
+        <form className='my-6' onSubmit={handleSubmit}>
+            <div className='md:max-w-[500px] flex items-center justify-between gap-2 border border-gray-300 rounded py-1 px-2 mx-auto bg-transparent'>
+                <Search className='text-gray-400' />
+                <input type="text" className='flex-1 outline-0 text-sm md:text-[16px] text-gray-600' placeholder='Search for Courses..' value={searchInput} onChange={(e) => setSearchInput(e.target.value)} />
+                <button className='py-2 px-6 btn-bg text-white rounded-sm cursor-pointer' type='submit'>Search</button>
+            </div>
+        </form>
     )
 }
 
--- client/src/pages/student/Home.jsx
+++ client/src/pages/student/Home.jsx
@@ -8,6 +8,16 @@ const Home = () => {
         <div className='h-screen'>
             <Navbar />
             <Hero />
+            <main className='px-10 md:px-30 py-10 mx-auto'>
+                {/*find course-search bar */}
+                <section className=''>
+                    <div className=''>
+                        <h1 className='font-roboto text-2xl text-gray-700 md:text-3xl font-medium text-center'>Find Your Perfect Course</h1>
+                        <p className='font-roboto text-lg md:text-2xl font-light text-center md:max-w-[900px] mx-auto mt-4'>Find the right course to upgrade your skills, enhance your knowledge, and move closer to your personal or professional goals.</p>
+                        <SearchBar />
+                    </div>
+                </section>
+            </main>
         </div>
     )
 }
