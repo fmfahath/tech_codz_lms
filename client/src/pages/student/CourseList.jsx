d31e5b30cc0a43220cc07c8658a3f3981b84c640
fmfahath <fmfahath.dev@gmail.com>
2025-07-23 00:40:24 +0530

    client -- Popular Course section added in Home.jsx, CourseList page layout created, SearchBar component added in CourseList.jsx

--- client/src/pages/student/CourseList.jsx
+++ client/src/pages/student/CourseList.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import SearchBar from '../../components/student/SearchBar'
+import { dummyCourses } from '../../assets/assets'
+import CourseCard from '../../components/student/CourseCard'
 
 const CourseList = () => {
     return (
-        <div>CourseList</div>
+        <div className='px-10 md:px-30 py-30  '>
+            <div className='mb-5 md:mb-10 '>
+                <p className='text-lg md:text-xl text-center text-gray-700'>Search by Course Names</p>
+                <SearchBar />
+            </div>
+            <hr className='border border-gray-100' />
+            <div>
+
+            </div>
+        </div>
     )
 }
 
--- client/src/pages/student/Home.jsx
+++ client/src/pages/student/Home.jsx
@@ -11,10 +11,21 @@ const Home = () => {
             <main className='px-10 md:px-30 py-10 mx-auto'>
                 {/*find course-search bar */}
                 <section className=''>
-                    <div className=''>
-                        <h1 className='font-roboto text-2xl text-gray-700 md:text-3xl font-medium text-center'>Find Your Perfect Course</h1>
-                        <p className='font-roboto text-lg md:text-xl font-light text-center md:max-w-[700px] mx-auto mt-4'>Find the right course to upgrade your skills, enhance your knowledge, and move closer to your personal or professional goals.</p>
+                    <div className='space-y-10'>
+                        <div>
+                            <h1 className='font-roboto text-2xl text-gray-700 md:text-3xl font-medium text-center'>Find Your Perfect Course</h1>
+                            <p className='font-roboto text-lg md:text-xl font-light text-center md:max-w-[700px] mx-auto mt-4'>Find the right course to upgrade your skills, enhance your knowledge, and move closer to your personal or professional goals.</p>
+                        </div>
                         <SearchBar />
+                    </div>
+                </section>
+
+                {/* /popular courses */}
+                <section>
+                    <h2 className='text-xl md:text-2xl font-medium text-gray-700 mt-10'>Popular Courses</h2>
+                    <hr className='border border-gray-100' />
+                    <div>
+
                     </div>
                 </section>
             </main>
