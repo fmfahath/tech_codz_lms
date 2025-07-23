00cc879ae601d3fd178ecf9e9ab9b2943d839009
fmfahath <fmfahath.dev@gmail.com>
2025-07-23 22:19:16 +0530

    client -- main section removed, sections layout modified in Home.jsx+

--- client/src/pages/student/Home.jsx
+++ client/src/pages/student/Home.jsx
@@ -34,24 +34,24 @@ const Home = () => {
 
     return (
         <div className='h-screen'>
-            {/* <Navbar /> */}
             <Hero />
-            <main className='px-10 md:px-30 py-10 mx-auto'>
-                {/*find course-search bar */}
-                <section className='py-4 md:py-10'>
-                    <div className='flex flex-col md:flex-row items-center justify-start gap-5'>
-                        <div className='md:max-w-[600px] '>
-                            <h1 className='font-roboto text-2xl text-gray-700 md:text-3xl font-medium text-center md:text-start'>Find Your Perfect Course</h1>
-                            <p className='font-roboto text-lg md:text-xl font-light text-center md:text-start mx-auto mt-4'>Find the right course to upgrade your skills, enhance your knowledge, and move closer to your personal or professional goals.</p>
-                        </div>
-                        <div className='flex-1 block '>
-                            <SearchBar />
-                        </div>
-                    </div>
-                </section>
 
-                {/* popular courses */}
-                <section className='bg-gray-50 p-5 rounded-md shadow-lg'>
+            {/*find course-search bar */}
+            <section className='px-10 md:px-30 py-4 md:py-10'>
+                <div className='flex flex-col md:flex-row items-center justify-start gap-5'>
+                    <div className='md:max-w-[600px] '>
+                        <h1 className='font-roboto text-2xl text-gray-700 md:text-3xl font-medium text-center md:text-start'>Find Your Perfect Course</h1>
+                        <p className='font-roboto text-lg md:text-xl font-light text-center md:text-start mx-auto mt-4'>Find the right course to upgrade your skills, enhance your knowledge, and move closer to your personal or professional goals.</p>
+                    </div>
+                    <div className='flex-1 block '>
+                        <SearchBar />
+                    </div>
+                </div>
+            </section>
+
+            {/* popular courses */}
+            <section className='px-10 md:px-30 '>
+                <div className='bg-gray-50 p-5 rounded-md shadow-lg'>
                     <h2 className='text-xl md:text-2xl font-medium text-gray-700 mt-5'>Popular Courses</h2>
                     {/* <hr className='border border-gray-100' /> */}
                     {allCourses && allCourses.length > 0 ? (
@@ -71,26 +71,28 @@ const Home = () => {
                             {/* <div className="absolute top-0 right-0 h-full w-30 bg-gradient-to-l from-white to-transparent pointer-events-none z-10" /> */}
                         </div>
                     ) : "No courses at the moment"}
-                </section>
+                </div>
+            </section>
 
-                {/* latest courses */}
-                <section className='mt-5 md:mt-10'>
-                    <h2 className='text-xl md:text-2xl font-medium text-gray-700 mt-5'>Latest Courses</h2>
-                    <hr className='border border-gray-100' />
-                    {allCourses && allCourses.length > 0 ? (
-                        <div className=''>
-                            <div className='grid grid-cols-[repeat(auto-fit,_minmax(100px,_1fr))] md:grid-cols-[repeat(auto-fit,_minmax(250px,_1fr))] gap-2 md:gap-4 mt-5'>
-                                {allCourses.slice(0, 8).map(course => <CourseCard key={course._id} courseData={course} />).reverse()}
-                            </div>
+            {/* latest courses */}
+            <section className='px-10 md:px-30 mt-5 md:mt-10'>
+                <h2 className='text-xl md:text-2xl font-medium text-gray-700 mt-5'>Latest Courses</h2>
+                <hr className='border border-gray-100' />
+                {allCourses && allCourses.length > 0 ? (
+                    <div className=''>
+                        <div className='grid grid-cols-[repeat(auto-fit,_minmax(100px,_1fr))] md:grid-cols-[repeat(auto-fit,_minmax(250px,_1fr))] gap-2 md:gap-4 mt-5'>
+                            {allCourses.slice(0, 8).map(course => <CourseCard key={course._id} courseData={course} />).reverse()}
                         </div>
-                    ) : "No courses at the moment"}
-                    {/* expolre button */}
-                    <div className=' mt-2 md:mt-10 relative '>
-                        <div className='w-full h-[200px] absolute -top-50 left-0 bg-gradient-to-t from-white via-white/80 to-transparent z-10'></div>
-                        <Link to='/course-list' className='w-max mx-auto flex items-center justify-between gap-2 btn-bg py-2 px-4  md:text-lg scroll-smooth'>Explore More <RiArrowRightDoubleLine /></Link>
                     </div>
-                </section>
-            </main>
+                ) : "No courses at the moment"}
+                {/* expolre button */}
+                <div className=' mt-2 md:mt-10 relative '>
+                    <div className='w-full h-[200px] absolute -top-50 left-0 bg-gradient-to-t from-white via-white/80 to-transparent z-10'></div>
+                    <Link to='/course-list' className='w-max mx-auto flex items-center justify-between gap-2 btn-bg py-2 px-4  md:text-lg scroll-smooth'>Explore More <RiArrowRightDoubleLine /></Link>
+                </div>
+            </section>
+
+
         </div>
     )
 }
