00419a0390fe887ccf8996d795c415284b35ef76
fmfahath <fmfahath.dev@gmail.com>
2025-07-24 13:12:31 +0530

    client -- search filter, search key word tag logic added in CourseList.jsx

--- client/src/pages/student/CourseList.jsx
+++ client/src/pages/student/CourseList.jsx
@@ -1,13 +1,32 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import SearchBar from '../../components/student/SearchBar'
 import { dummyCourses } from '../../assets/assets'
 import CourseCard from '../../components/student/CourseCard'
 import { AppContext } from '../../context/AppContext'
 import Loading from '../../components/student/Loading'
+import { useNavigate, useParams } from 'react-router-dom'
+import { IoMdCloseCircle } from "react-icons/io";
 
 const CourseList = () => {
 
     const { allCourses } = useContext(AppContext)
+    const [filteredCourses, setFilteredCourses] = useState([])
+    const { input } = useParams()
+    const navigate = useNavigate()
+
+
+
+    //filter courses based on search
+    useEffect(() => {
+        if (allCourses && allCourses.length > 0) {
+            const tempCourses = allCourses.slice()
+
+            input ? setFilteredCourses(tempCourses.filter(course =>
+                course.courseTitle.toLowerCase().includes(input.toLowerCase())
+            )) : setFilteredCourses(tempCourses)
+        }
+    }, [input, allCourses])
+
 
     return (
         <div className='px-10 md:px-30 py-30'>
@@ -18,9 +37,12 @@ const CourseList = () => {
                 </div>
             </div>
             <hr className='border border-gray-100' />
+            {input && <div className='my-2'>
+                <p className='text-gray-700'>Search word: <span className='inline-flex items-center gap-1 py-1 px-2 bg-gray-300/70 hover:bg-red-200 rounded-md shadow-md cursor-pointer'>{input}<IoMdCloseCircle className='text-red-500 cursor-pointer' onClick={() => navigate('/course-list')} /></span></p>
+            </div>}
             <div className='grid grid-cols-[repeat(auto-fit,_minmax(100px,_1fr))] md:grid-cols-[repeat(auto-fit,_minmax(250px,_1fr))] gap-2 md:gap-4  mt-5'>
                 {
-                    allCourses ? allCourses.map((course, i) => <CourseCard key={i} courseData={course} />) : <Loading />
+                    filteredCourses ? filteredCourses.map((course, i) => <CourseCard key={i} courseData={course} />) : <Loading />
                 }
             </div>
         </div>
