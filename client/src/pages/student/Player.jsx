29f005d6cffc55aa4ea0e0bfb0f1b9b1aad4d6b6
fmfahath <fmfahath.dev@gmail.com>
2025-08-08 08:06:38 +0530

    client -- chapter content menu toggle func, animation added in Player component

--- client/src/pages/student/Player.jsx
+++ client/src/pages/student/Player.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import { AppContext } from '../../context/AppContext'
 import humanizeDuration from 'humanize-duration'
 import { IoIosArrowDown } from "react-icons/io";
@@ -11,11 +11,17 @@ const Player = () => {
     const { courseId } = useParams()
     const { allCourses } = useContext(AppContext)
     const [courseDetails, setCourseDetails] = useState(null)
+    const [toggle, setToggle] = useState({})
 
     //getCourseDetails
     const getCourseDetails = () => {
         const courseData = allCourses.find(course => course._id === courseId)
         setCourseDetails(courseData)
+    }
+
+    //toggle menu content - show/hide
+    const toggleSection = (index) => {
+        setToggle(prev => ({ ...prev, [index]: !prev[index] }))
     }
 
     useEffect(() => {
@@ -35,21 +41,21 @@ const Player = () => {
                     <div>
                         {courseDetails?.courseContent.map((chapter, index) => (
                             <div key={index} className='bg-gray-500/5 mb-2 px-4 py-2 shadow rounded'>
-                                <div key={index} className='flex items-center justify-start gap-2 cursor-pointer'>
-                                    <IoIosArrowDown />
+                                <div className='flex items-center justify-start gap-2 cursor-pointer' onClick={() => toggleSection(index)}>
+                                    <IoIosArrowDown className={`transform transition-transform ${toggle[index] ? "rotate-180" : ""}`} />
                                     <p className='font-medium'>{chapter.chapterTitle}</p>
                                 </div>
-                                <div className="">
+                                <div className={`${toggle[index] ? "max-h-auto" : "h-0"} overflow-hidden`}>
                                     {chapter.chapterContent.map((lecture, i) => (
-                                        <div key={i} className=' flex items-center justify-between pl-5 text-gray-700 mt-2'>
-                                            <div className='flex items-center gap-2'>
+                                        <div key={i} className=' flex items-center justify-between pl-5 text-gray-700 mt-2 '>
+                                            <div className='flex items-center gap-2 '>
                                                 {/* <IoCheckmarkDoneCircleOutline /> */}
                                                 <IoCheckmarkDoneCircleSharp className='text-blue-500' />
-                                                <p>{lecture.lectureTitle}</p>
+                                                <p className='truncate'>{lecture.lectureTitle}</p>
                                             </div>
-                                            <div className='flex items-center justify-start gap-5'>
-                                                <a href="#">Link</a>
-                                                <p>{humanizeDuration(lecture.lectureDuration * 60 * 1000, { units: ["h", "m"] })}</p>
+                                            <div className='w-40 flex items-center gap-5 '>
+                                                <Link className='text-blue-500'>Play</Link>
+                                                <p className='truncate'>{humanizeDuration(lecture.lectureDuration * 60 * 1000, { units: ["h", "m"] })}</p>
                                             </div>
                                         </div>
                                     ))}
