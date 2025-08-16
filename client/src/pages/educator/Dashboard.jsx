6c40d60015256e368061d348bc8a3c52f043b3b8
fmfahath <fmfahath.dev@gmail.com>
2025-08-16 08:28:11 +0530

    client -- dashboard layout created

--- client/src/pages/educator/Dashboard.jsx
+++ client/src/pages/educator/Dashboard.jsx
@@ -1,9 +1,63 @@
 import React from 'react'
+import { FaUsers } from "react-icons/fa";
+import { ImBooks } from "react-icons/im";
+import { RiMoneyDollarCircleFill } from "react-icons/ri";
 
 const Dashboard = () => {
     return (
-        <div className=''>
-            dashboard
+        <div className='w-full min-h-screen px-4 py-2'>
+            <div className='flex items-center  gap-5 mb-10'>
+                <div className='flex flex-col md:flex-row items-center gap-5 shadow-md px-4 py-2 rounded-lg btn-bg mb'>
+                    <FaUsers className='w-15 h-15 text-white' />
+                    <div>
+                        <p className='font-medium text-2xl md:text-5xl'>10</p>
+                        <p className='text-lg md:text-xl'>Students Enrolled</p>
+                    </div>
+                </div>
+                <div className='flex items-center gap-5 shadow-md px-4 py-2 rounded-lg btn-bg'>
+                    <ImBooks className='w-15 h-15 text-white' />
+                    <div>
+                        <p className='font-medium text-2xl md:text-5xl'>03</p>
+                        <p className='text-lg md:text-xl'>Total Courses</p>
+                    </div>
+                </div>
+                <div className='flex items-center gap-5 shadow-md px-4 py-2 rounded-lg btn-bg'>
+                    <RiMoneyDollarCircleFill className='w-15 h-15 text-white' />
+                    <div>
+                        <p className='font-medium text-2xl md:text-5xl'>$123.45</p>
+                        <p className='text-lg md:text-xl'>Total Earnings</p>
+                    </div>
+                </div>
+            </div>
+
+            {/* table */}
+            <div className=''>
+                <h3 className='text-xl '>Latest Enrollments</h3>
+                <table className='w-full mt-5 border border-gray-200 shadow rounded-lg'>
+                    <thead>
+                        <tr className='text-left border-b border-gray-200'>
+                            <th className='px-4 py-4  font-medium'>#</th>
+                            <th className='px-4 py-4 font-medium'>Student Name</th>
+                            <th className='px-4 py-4 font-medium'>Course Title</th>
+                            <th className='px-4 py-4  font-medium'>Date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr className='text-left text-gray-600 border-b border-gray-200'>
+                            <td className='px-4 py-2 '>1</td>
+                            <td className='px-4 py-2 '>Fahath</td>
+                            <td className='px-4 py-2 '>React for Beginners</td>
+                            <td className='px-4 py-2 '>2023-10-01</td>
+                        </tr>
+                        <tr className='text-left text-gray-600 border-b border-gray-200'>
+                            <td className='px-4 py-2 '>2</td>
+                            <td className='px-4 py-2 '>David</td>
+                            <td className='px-4 py-2 '>Python for Data SCience</td>
+                            <td className='px-4 py-2 '>2023-10-01</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 }
