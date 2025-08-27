9aad1992c10a15fd76c489819a5e0e23d11a3e20
fmfahath <fmfahath.dev@gmail.com>
2025-08-27 21:39:28 +0530

    client -- clerk account created, clerk npm installed, clerk publishable_key added .env, clerk_provider added in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,21 @@ import './index.css'
 import App from './App.jsx'
 import { AppCondextProvider } from './context/AppContext.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { ClerkProvider } from '@clerk/clerk-react'
+
+// Import your Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <AppCondextProvider>
-      <App />
-    </AppCondextProvider>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <AppCondextProvider>
+        <App />
+      </AppCondextProvider>
+    </ClerkProvider>
   </BrowserRouter>,
 )
