09c96b47b716d3e50243798dc7905e3fbb060cfe
fmfahath <fmfahath.dev@gmail.com>
2025-08-28 22:14:03 +0530

    server -- clerkWebhooks controller added webhooks.js, clerk route added in server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import connectDB from "./configs/mongodb.js";
+import { clerkWebhooks } from "./contollers/webhooks.js";
 
 //initialize express
 const app = express();
@@ -10,10 +11,12 @@ const app = express();
 connectDB()
 
 //middleware
+app.use(express.json())
 app.use(cors())
 
 //routes
 app.get('/', (req, res) => res.send("API is running..."))
+app.post('/clerk', clerkWebhooks)
 
 //port
 const PORT = process.env.PORT || 5000
