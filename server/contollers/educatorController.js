c4811f35a5eb3d637e90160482d13e9538466e19
fmfahath <fmfahath.dev@gmail.com>
2025-09-04 11:19:53 +0530

    client -- getAuth, useUser hooks imported, getToken func added in AppContext, updateToEducatorRole route API test success

--- server/contollers/educatorController.js
+++ server/contollers/educatorController.js
@@ -2,7 +2,7 @@
 import { clerkClient } from '@clerk/express'
 
 //update role from student to educator----------------------------------------------------
-export const updateRoleToEducator = async () => {
+export const updateRoleToEducator = async (req, res) => {
     try {
         const userId = req.auth.userId;
 
