5ec0c3906e490c1e1e98b8f574849a1fc0afeb00
fmfahath <fmfahath.dev@gmail.com>
2025-09-11 13:32:13 +0530

    server -- getEducatorDashboardData, getEnrolledStudentsData controller func crrated, routes added

--- server/contollers/educatorController.js
+++ server/contollers/educatorController.js
@@ -2,6 +2,8 @@
 import { clerkClient } from '@clerk/express'
 import courseModel from '../models/courseModel.js';
 import { v2 as cloudinary } from "cloudinary";
+import purchaseModel from '../models/purchaseMOdel.js';
+import userModel from '../models/userModel.js';
 
 //update role from student to educator----------------------------------------------------
 export const updateRoleToEducator = async (req, res) => {
@@ -52,6 +54,68 @@ export const getEducatorCourses = async (req, res) => {
         const educatorCourses = await courseModel.find({ educator: educatorId })
 
         res.json({ success: true, courses: educatorCourses })
+    } catch (error) {
+        res.json({ success: false, message: error.message })
+    }
+}
+
+
+//get educator dashboard data----------------------------------------------------
+export const getEducatorDashboardData = async (req, res) => {
+    try {
+        const educatorId = req.auth.userId
+        const educatorCourses = await courseModel.find({ educator: educatorId })
+        const educatorTotalCourses = educatorCourses.length
+        const educatorCoursesIds = educatorCourses.map(course => course._id)
+
+        //total earnings
+        const purchases = await purchaseModel.find({ courseId: { $in: educatorCoursesIds }, status: 'completed' })
+        const EducatorTotalEarnings = purchases.reduce((acc, purchase) => acc + purchase.amount, 0)
+
+        //enrolles students data
+        const enrolledStudentsData = []
+        for (const course of educatorCourses) {
+            const students = await userModel.find({
+                _id: { $in: course.enrolledStudents }
+            }, 'name imageUrl')
+
+            students.forEach(student => {
+                enrolledStudentsData.push({
+                    courseTitle: course.courseTitle,
+                    student
+                })
+            })
+        }
+
+        res.json({
+            success: true, dashboardData: {
+                EducatorTotalEarnings, enrolledStudentsData, educatorTotalCourses
+            }
+        })
+
+    } catch (error) {
+        res.json({ success: false, message: error.message })
+    }
+}
+
+//get enrolled students data---------------------------------------------------------
+export const getEnrolledStudentsData = async (req, res) => {
+    try {
+        const educatorId = req.auth.userId
+        const educatorCourses = await courseModel.find({ educator: educatorId })
+        const educatorCoursesIds = educatorCourses.map(course => course._id)
+
+        const purchases = await purchaseModel.find({ courseId: { $in: educatorCoursesIds }, status: 'completed' })
+            .populate('userId', 'name imageUrl').populate('courseId', 'courseTitle')
+
+        const enrolledStudentsData = purchases.map(purchase => ({
+            student: purchase.userId,
+            courseTitle: purchase.courseId.courseTitle,
+            purchaseDate: purchase.createdAt
+        }))
+
+        res.json({ success: true, enrolledStudentsData })
+
     } catch (error) {
         res.json({ success: false, message: error.message })
     }
--- server/routes/educatorRoutes.js
+++ server/routes/educatorRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addNewCourse, getEducatorCourses, updateRoleToEducator } from "../contollers/educatorController.js";
+import { addNewCourse, getEducatorCourses, getEducatorDashboardData, getEnrolledStudentsData, updateRoleToEducator } from "../contollers/educatorController.js";
 import { verifyEducatorRole } from "../middlewares/authMiddleware.js";
 import upload from "../configs/multer.js";
 
@@ -8,6 +8,8 @@ const educatorRouter = express.Router();
 educatorRouter.get('/update-role', updateRoleToEducator)
 educatorRouter.post('/add-course', verifyEducatorRole, upload.single('image'), addNewCourse)
 educatorRouter.get('/courses', verifyEducatorRole, getEducatorCourses)
+educatorRouter.get('/dashboard-data', verifyEducatorRole, getEducatorDashboardData)
+educatorRouter.get('/enrolled-students-data', verifyEducatorRole, getEnrolledStudentsData)
 
 export default educatorRouter;
 
