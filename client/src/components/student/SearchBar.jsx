0fc839772be8357ca6cef97e7e025ca548bdba31
fmfahath <fmfahath.dev@gmail.com>
2025-09-30 14:57:11 +0530

    client -- fix >> mobile responsive - search box layout fixed in Home.jsx

--- client/src/components/student/SearchBar.jsx
+++ client/src/components/student/SearchBar.jsx
@@ -14,11 +14,11 @@ const SearchBar = () => {
     }
 
     return (
-        <form className='w-full md:w-auto my-2 bg-green-200' onSubmit={handleSubmit}>
+        <form className='w-full md:w-auto my-2 ' onSubmit={handleSubmit}>
             <div className='w-full md:max-w-[500px] flex items-center md:justify-between gap-2 border border-gray-300 rounded py-1 px-2 mx-auto bg-transparent'>
                 <Search className='text-gray-400 shrink-0' />
                 <input type="text" className='w-full flex-1 outline-0 text-sm md:text-[16px] text-gray-600' placeholder='Search for Courses..' value={searchInput} onChange={(e) => setSearchInput(e.target.value)} />
-                <button className='py-2 px-4 md:px-6 btn-bg text-white rounded-sm cursor-pointer shrink-0' type='submit'>Search</button>
+                <button className='py-2 px-3 md:px-6 btn-bg text-white rounded-sm cursor-pointer shrink-0' type='submit'>Search</button>
             </div>
         </form>
     )
